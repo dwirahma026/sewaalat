69c6b87e3a4c7dee9f32a8247849b8d2d8765ffb
dwirahma026 <dwir7882@gmail.com>
2024-03-07 20:22:32 +0700

    update dwlogin views

--- resources/views/dwlogin.blade.php
+++ resources/views/dwlogin.blade.php
@@ -1,3 +1,5 @@
+<html>
 <div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+    <h1>ini dwi<h1>
 </div>
+</html>
