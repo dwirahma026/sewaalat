3ce6215043e781f203fac2a7b63f142eec18ab2f
dwirahma026 <dwir7882@gmail.com>
2024-06-01 17:07:28 +0800

    update daftar dw

--- resources/views/dwdaftar.blade.php
+++ resources/views/dwdaftar.blade.php
@@ -3,27 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inknut+Antiqua&display=swap" rel="stylesheet">
+    <title>DAFTAR</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <div class="bg-lime-400 w-full h-18">
+        <div>
+            <div class="flex items-center">
+                <div class="font-bold px-4 py-5">
+                    <a href="daftar.html">Daftar</a>
+                </div>
+                <div class="font-bold px-4 py-5">
+                    <a href="login.html">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-screen flex-col">
     <div class="py-3 text-regular text-center text-xl tracking-widest">Logo</div>
-    <main class="bg-yellow-100 max-w-lg mx-auto p-2 rounded-lg shadow-2xl">
-        <div class="py-3 font-serif text-regular text-center text-xl tracking-widest uppercase">DAFTAR</div>
-
-        <section class="mt-6">
-            <form action="" class="flex flex-col">
-                <div><input class="bg-transparent w-full text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2" type="text" placeholder="Username"></div> </br>
-                <div><input class="bg-transparent w-full text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2" type="password" placeholder="password"></div> </br>
-                <div><input class="bg-transparent w-full text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2" type="text" placeholder="NAMA"></div> </br>
-                <div><input class="bg-transparent w-full text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2" type="text" placeholder="NOMOR HP"></div> </br>
-                <div><input class="bg-transparent w-full text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2" type="text" placeholder="ALAMAT"></div> </br>
-                <button class="bg-yellow-300 w-200 text-black rounded-r-full rounded-l-full shadow-lg hover:shadow-xl transition duration-500 font-bold py-3" type="submit">DAFTAR</button> </br>
+    <div class="bg-yellow-100 w-1/4 p-2 shadow-2xl">
+        <div class="py-3 font-serif text-center text-xl tracking-widest uppercase">DAFTAR</div>
+        <div>
+            <form action="" class="flex flex-col items-center">
+                <div class="flex bg-transparent text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-6">
+                        <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                      </svg>                    
+                  <input class="bg-transparent" type="text" placeholder="Username">
+                </div> </br>
+                <div class="flex bg-transparent text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-6">
+                        <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clip-rule="evenodd" />
+                      </svg>                                            
+                    <input class="bg-transparent" type="password" placeholder="password">
+                </div> </br>
+                <div class="flex bg-transparent text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-6">
+                        <path fill-rule="evenodd" d="M4.5 3.75a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V6.75a3 3 0 0 0-3-3h-15Zm4.125 3a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Zm-3.873 8.703a4.126 4.126 0 0 1 7.746 0 .75.75 0 0 1-.351.92 7.47 7.47 0 0 1-3.522.877 7.47 7.47 0 0 1-3.522-.877.75.75 0 0 1-.351-.92ZM15 8.25a.75.75 0 0 0 0 1.5h3.75a.75.75 0 0 0 0-1.5H15ZM14.25 12a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H15a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3.75a.75.75 0 0 0 0-1.5H15Z" clip-rule="evenodd" />
+                      </svg>                                           
+                    <input class="bg-transparent" type="text" placeholder="NAMA">
+                </div> </br>
+                <div class="flex bg-transparent text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-6">
+                        <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+                      </svg>                                           
+                    <input class="bg-transparent" type="text" placeholder="NOMOR HP">
+                </div> </br>
+                <div class="flex bg-transparent text-black focus:outline-none border-black border-b-2 border-t-2 border-l-2 border-r-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-6">
+                        <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+                      </svg>                                           
+                    <input class="bg-transparent" type="text" placeholder="ALAMAT">
+                </div> </br>
+                <button class="bg-yellow-300 w-40 text-black rounded-r-full rounded-l-full shadow-lg hover:shadow-xl transition duration-500 font-bold py-3" type="submit">DAFTAR</button> </br>
             </form>
-        </section>
-    </main>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
